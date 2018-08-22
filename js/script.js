547237154eab077c89040673493d9f0a06ac326c
MEVEN <meven.ameline@gmail.com>
2018-08-23 00:26:25 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var full = document.querySelector('#full')
+var haut = document.querySelector('#haut')
+var bas = document.querySelector('#bas')
+var text = document.querySelector('#text')
+var name = document.querySelector('#name')
+var loop;
+
+setTimeout(function(){writeText("#name", "Caligula", 50, 0)},1000);
+
+function writeText(target, message, delay, i) {
+    
+    if (i < message.length) {
+        if (message[i]==='£'){
+            document.querySelector(target).append('<br/>');
+        }
+        else{
+            document.querySelector(target).append(message[i]);
+        }
+        
+        i++;
+        loop=setTimeout(function(){writeText(target, message, delay, i)}, delay);
+    }
+}
+
+function over() {
+    writeText("#text", "Qu'ils me haïssent,£ pourvu qu'ils me craignent.", 30, 0);
+}
+function out() {    
+    text.textContent = "";
+    clearTimeout(loop); 
+}
